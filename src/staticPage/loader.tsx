1db23121cec1278b326b10bab18044265b7a515b
hardySin <deep16hardy@gmail.com>
2021-11-29 00:42:55 +0530

    commit to loader and view set on admin panel

---
+++ src/staticPage/loader.tsx
@@ -0,0 +1,15 @@
+import "../css/loader.css";
+import loop from '../images/loop.gif';
+
+const Loader = (): any => {
+
+    return (
+        <>
+        <div className="loader">
+         </div>
+         </>
+    )
+}
+
+
+export default Loader;
